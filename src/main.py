5685607aaad791c3d7081646c988549dc34fd149
SovietBeast <pawlik1998@gmail.com>
2022-11-27 23:13:37 +0100

    another try with cors

--- src/main.py
+++ src/main.py
@@ -23,7 +23,7 @@ origins = [
     "http://localhost:5173",
     "http://127.0.0.1",
     "http://127.0.0.1:5173",
-    "https://chatter-io.netlify.app/"
+    "https://chatter-io.netlify.app"
 ]
 
 app.add_middleware(
